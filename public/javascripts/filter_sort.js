21e727052b8b4525123835a6d961d203680be077
PaulyB <borregopaulj@gmail.com>
2025-05-01 10:19:00 -0500

    hello hotfix

--- public/javascripts/filter_sort.js
+++ public/javascripts/filter_sort.js
@@ -10,6 +10,10 @@ let teambuilder = 0;
 let teams = [];
 let data = null;
 
+if (window.location.href.includes("teambuilder")) {
+    teambuilder = 1;
+}
+
 async function loadHandler(event) {
     const response = await getStatus();
     if (response.ok) {
@@ -94,13 +98,9 @@ function postTeam() {
     xhr.responseType = "";
     xhr.open("POST", "/addToTeam");
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded" );
-    console.log("Adding to teams... " + teams);
     xhr.send(`teamname="blank"&pid1=${teams[0]}&pid2=${teams[1]}&pid3=${teams[2]}&pid4=${teams[3]}&pid5=${teams[4]}&pid6=${teams[5]}&user=${data.user.username}`);
     
     sleep(100).then(() => { window.location.href = "/trainer"; });
-}
-function team() {
-    teambuilder = 1;
 }
 
 function addToTeams(pid) {
